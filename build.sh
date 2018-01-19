3b55d4f84c0c8e6534056f1b6feaf77360d8513f
nmanumr <naumanumer>
2018-01-19 14:14:36 +0500

    added phy-09

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 if [ -z "$1" ]
 then
-  echo "Which folder do you want to deploy to GitHub Pages?"
+  echo "Please enter commit string"
   exit 1
 fi
-git subtree push --prefix $1 origin gh-pages
+sed -i "s/#  disqus: 'fung-notes'/  disqus: 'fung-notes'/g" mkdocs.yml
+python -m mkdocs build
+git add site
+git commit -m "$1 - build"
+git subtree push --prefix site origin gh-pages
+sed -i "s/  disqus: 'fung-notes'/#  disqus: 'fung-notes'/g" mkdocs.yml
