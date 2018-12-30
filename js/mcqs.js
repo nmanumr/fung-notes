875f737c323c8a4320ca9ec293a9bc62ada9db12
nmanumr <u.nauman@gmail.com>
2018-12-30 17:39:08 +0500

    added mocktests

--- js/mcqs.js
+++ js/mcqs.js
@@ -1,14 +1,16 @@
-const unitQuestionTime = 54; //sec
 
+const unitQuestionTime = 54; //sec
 
 var sec = calcTestTime(); // Calculate test total time
 var interval = setInterval(countDown, 1000); // main countDown interval
 var currentQ; // Current Question in view
 var submitted=false;
+var total_questions = document.querySelectorAll(".questions li").length
+var header = document.querySelector(".md-header .md-flex")
 
 // Apend Sidebar to DOM
 var questions_list;
-for(var i=1; i<=document.querySelectorAll(".questions li").length; i++){
+for(var i=1; i<=total_questions; i++){
   var num;
   if(i<10) num="0"+i;
   else num =i;
@@ -18,17 +20,30 @@ for(var i=1; i<=document.querySelectorAll(".questions li").length; i++){
 var tocInner = document.querySelector('.md-sidebar--secondary .md-sidebar__inner');
 tocInner.innerHTML =`<div class="side-nav">
     <div class="text-right timer-div">
-      <input type="checkbox" id="id-name--1" name="set-name" class="switch-input" checked onchange='handleTimerSwitch(this)'>
-    	<label for="id-name--1" class="switch-label">Timer <span class="toggle--on">On</span><span class="toggle--off">Off</span></label>
+      <input type="checkbox" id="id-name--1" name="set-name" class="switch-input" checked onchange='toggleTimer(this)'>
+    	<label for="id-name--1" class="switch-label"><span>Timer </span><span class="toggle--on">On</span><span class="toggle--off">Off</span></label>
     </div>
-    <div class="timer"><span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep">h</span><span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep">m</span><span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep" style="padding-right:0px">s</span></div>
+    <div class="timer text-center"><span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep">h</span><span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep">m</span><span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep" style="padding-right:0px">s</span></div>
+    <div class="score hidden text-center">
+      <span class="act-tim-digit" tabindex="0">00</span><span class="act-tim-sep">of</span><span class="act-tim-digit" tabindex="0">00</span>
+    </div>
     <div class="toolbar">
-      <span class="label">Goto:</span> <select class="goto-select" onchange="scrollToQ(+this.value)">
+      <span class="goto label">Goto:</span> <select class="goto-select" onchange="scrollToQ(+this.value)">
       ${questions_list}
       </select>
       <button class="submit-btn" onclick="submitTest()">Submit</button>
     </div>
   </div>`
+
+
+// add timer toggle to header
+// header.innerHTML = header.innerHTML + `
+//   <div class="md-flex__cell md-flex__cell--shrink">
+//     <label class="md-icon md-icon--timer md-header-nav__button"
+//           title="toggle timer" onclick="toggleTimer(this)"
+//     ></label>
+//   </div>
+// `
 
 
 /**
@@ -51,18 +66,21 @@ function secTohms(s){
 /** Write time to DOM */
 function renderTime(time){
     var timerSpans = document.querySelectorAll(".timer .act-tim-digit");
+
     if (time[0]>-1){ // hour
       if (time[0]>9)
         timerSpans[0].innerHTML=time[0]
       else
         timerSpans[0].innerHTML="0"+time[0];
     }
+
     if (time[1]>-1){ // min
       if (time[1]>9)
         timerSpans[1].innerHTML=time[1]
       else
         timerSpans[1].innerHTML="0"+time[1];
     }
+
     if (time[2]>-1){ // sec
       if (time[2]>9)
         timerSpans[2].innerHTML=time[2]
@@ -84,7 +102,7 @@ function countDown(){
 }
 
 /** Timer Switch State change handler */
-function handleTimerSwitch(e){
+function toggleTimer(e){
     if(e.checked == true){
       interval = setInterval(countDown, 1000);
     } else {
@@ -101,20 +119,46 @@ function scrollToQ(n){
 function submitTest(){
     submitted = true;
     var radios = document.querySelectorAll(".md-radio");
+    var correct = 0, wrong=0;
+
     for(var i=0; i<radios.length; i++){
         var radio = radios[i];
         radio.children[0].disabled=true;
         var isChecked = radio.children[0].checked;
         var isCorrect = radio.classList.contains("correct");
-        if(isChecked && isCorrect)
-            radio.children[1].classList.add("correct");
-        else if(isChecked && !isCorrect)
-            radio.children[1].classList.add("wrong")
+
+        if(isChecked && isCorrect){
+          radio.children[1].classList.add("correct");
+          correct++;
+        }
+        else if(isChecked && !isCorrect){
+          radio.children[1].classList.add("wrong")
+          wrong++;
+        }
     }
+
     var anss = document.querySelectorAll(".ans");
     for(var i=0; i<anss.length; i++){
         anss[i].classList.remove('hidden');
         anss[i].classList.add('show');
     }
+
+    // hide timer
     clearInterval(interval);
+    document.getElementsByClassName('timer')[0].classList.add('hidden');
+
+    // show scores
+    document.getElementsByClassName('score')[0].classList.remove('hidden');
+    var spans = document.querySelectorAll(".score .act-tim-digit")
+    spans[0].innerText = correct;
+    spans[1].innerText = total_questions;
+}
+
+function checkAns(){
+  var nodes=document.querySelectorAll(".ans.show")
+  for(var i=0; i<nodes.length; i++){
+    if(nodes[i].innerText == "Ans: None"){
+      console.log(i);
+    }
+  }
 }
